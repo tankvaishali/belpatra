fdeac324d2423b61c0f37639e984558203c9ac86
tankvaishali <vaishalitank28603@gmail.com>
2025-02-28 17:55:21 +0530

    homecarousel set

--- src/Components/Home/HomeCarouselcom.jsx
+++ src/Components/Home/HomeCarouselcom.jsx
@@ -9,7 +9,9 @@ const slidesData = [
         sublineFirstLine: "Innovative Healing for a Healthier Tomorrow",
         bgImg: "https://media.post.rvohealth.io/wp-content/uploads/2020/08/Chamomile_and_Homeopathic_Medicine-1200x628-facebook-1200x628.jpg",
         rectImg: "https://previews.123rf.com/images/gordya/gordya1205/gordya120500003/13536595-white-pills-tablets-background-medicine-objects.jpg",
-        btntext: "WelCome to"
+        btntext: "WelCome to",
+        path:"/about",
+        pathbtn:"Know More"
     },
     {
         headlineFirstLine: "Quality Medicine",
@@ -17,7 +19,9 @@ const slidesData = [
         sublineFirstLine: "Quality Medicine for Better Health",
         bgImg: "https://www.ichthyol.de/wp-content/uploads/2022/09/Frau-mit-Mikroskop-im-Labor-1024x642.jpg",
         rectImg: "https://media.licdn.com/dms/image/C4E1BAQGm8nzf-4tx-g/company-background_10000/0/1584685478357/genchem_cover?e=2147483647&v=beta&t=GY-96OE6lUvfs6aQ0dPrRjGuvmUCKGliLAas_R7-Mms",
-        btntext: "Quality Medicine"
+        btntext: "Quality Medicine",
+        path:"/product",
+        pathbtn:"View More"
     },
     {
         headlineFirstLine: "Innovative Healing",
@@ -25,7 +29,9 @@ const slidesData = [
         sublineFirstLine: "Innovative Healing for a Brighter Future",
         bgImg: "https://belpatrapharmachem.com/wp-content/uploads/2022/06/photo1.jpg",
         rectImg: require("../../Assets/Images/borderimage.jpg"),
-        btntext: "Innovative Healing"
+        btntext: "Innovative Healing",
+        path:"/contact",
+        pathbtn:"Contact Us"
     }
 ];
 
@@ -95,7 +101,7 @@ const HomeCarouselcom = () => {
                                 <p>{slide.headlineFirstLine}</p>
                                 <p>{slide.headlineSecondLine}</p>
                             </h1>
-                            <Link to={"/about"} className="slide-content-cta" href="tel:7048313227"> <Buttoncom btn="Know More" /></Link>
+                            <Link to={slide.path} className="slide-content-cta" href="tel:7048313227"> <Buttoncom btn={slide.pathbtn} /></Link>
                         </div>
                         <h2 className="slide-side-text">
                             <span>{slide.sublineFirstLine}</span>
